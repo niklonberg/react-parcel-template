a87c5812267dbcde8adbb1302b6b9f5bc3b36a73
Nikolai <niklonberg@gmail.com>
2024-04-18 15:25:56 +0200

    fix dependencies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,8 @@
 import React from "react";
-import ToastProvider from "../ToastProvider";
-import ToastingGround from "../ToastingGround";
 import Footer from "../Footer";
 
 function App() {
-  return (
-    <ToastProvider>
-      <ToastingGround />
-      <Footer
-        text={
-          <p>
-            This is a challenge project as part of Joshua Comeau's{" "}
-            <b>Joy of React</b> course
-          </p>
-        }
-        address={
-          <address>
-            If you found this interesting, I'd love if you checked out my other
-            projects at{" "}
-            <a href="https://github.com/niklonberg">github.com/niklonberg</a>
-          </address>
-        }
-      />
-    </ToastProvider>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
